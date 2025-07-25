e2725e093724cfd7839d23cb5eff84c941b7aa47
Thiago-Weiss <thiagoweiss007@gmail.com>
2025-07-25 15:00:12 -0300

    att

--- app/api/endpoints/graficos/grafico.py
+++ app/api/endpoints/graficos/grafico.py
@@ -13,6 +13,27 @@ def obter_dados_grafico(
     data_fim: str = Query(...),
     coluna: str = Query(...)
 ):
+    """
+    estado          = tem que ser a string exata que é enviado no autocomplite para o front 
+    cidade          = tem que ser a string exata que é enviado no autocomplite para o front 
+    data_inicio     = formato YYYY-MM-DD
+    data_fim        = formato YYYY-MM-DD
+
+    coluna          = ainda falta fazer mais vai precisar de mais parametros pra montar ela mas vai ser algo assim
+                    pra cada coluna do grafico
+
+                    coluna= "precipitacao",     
+                    filtro= "soma",        
+                    hora_fixa= 23,                        
+                    janela_horas= (10, 12)
+                    
+                    a coluna e o filtro vai ser passado por endpoits para o front ou talvez hard code pq sao constantes
+                    e isso evitaria multiplos acessos ao back
+
+
+    """
+
+
     try:
         resultado = gerar_dados_grafico(
             estado= estado,
